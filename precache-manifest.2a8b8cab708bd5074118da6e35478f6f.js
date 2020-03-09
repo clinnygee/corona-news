ceb93056710af1cbd494894259c3a8b8fea76d35
clinnygee <clintongillespie@outlook.com>
2020-03-09 11:43:47 +1000

    Updates

---
+++ precache-manifest.2a8b8cab708bd5074118da6e35478f6f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cc9a7ca8602f91d330db3404a85e577",
+    "url": "/corona-news/index.html"
+  },
+  {
+    "revision": "015bba7925f09395aab5",
+    "url": "/corona-news/static/css/main.82b8fbde.chunk.css"
+  },
+  {
+    "revision": "d2892e9388f01274d2ee",
+    "url": "/corona-news/static/js/2.4e318e40.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/corona-news/static/js/2.4e318e40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "015bba7925f09395aab5",
+    "url": "/corona-news/static/js/main.f19ff447.chunk.js"
+  },
+  {
+    "revision": "9fd5b4af8b405e5483f5",
+    "url": "/corona-news/static/js/runtime-main.12faecca.js"
+  }
+]);
